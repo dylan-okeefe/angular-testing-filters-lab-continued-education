c7316dfab0ae6cc420013de51e32e220cd8e3ff0
dylan.okeefe <dylanokeefe@gmail.com>
2016-04-28 18:46:05 -0400

    done

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,26 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should filter female contacts', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'female';
+
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toBe(71);
+    });
+
+    it('should filter by geographical locations', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'manchester';
+
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toBe(8);
+    });
+    
 });
